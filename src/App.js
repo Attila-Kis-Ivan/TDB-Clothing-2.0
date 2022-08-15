f4606f315bf6b426a2b54dfc0b15ee12d9fac725
Attila <attila.kisivan@gmail.com>
2022-08-15 19:37:51 +0100

    updated directory page wit directory and category item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import CategoryItem from "./components/category-item/category-item.compponent";
+import Directory from "./components/directory/directory.component";
 
-import "./categories.styles.scss";
+
 
 const App = () => {
   const categories = [
@@ -30,13 +30,7 @@ const App = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
