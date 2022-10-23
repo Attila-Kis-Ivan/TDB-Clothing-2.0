0cda0b7282fc9d6d24691a1a44c4f2c720e5d5ed
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2022-10-23 09:48:17 +0100

    create user reducer and user selector

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,14 @@
 import { Fragment, useContext } from "react";
 
-import { Outlet, Link } from "react-router-dom";
+import { Outlet} from "react-router-dom";
+import { useSelector } from "react-redux"; 
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as TDBlogo } from "../../assets/TDB.svg";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentuser } from "../../store/user/user.selector";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
@@ -19,7 +20,7 @@ import {
 } from "./navigation.styles";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+ const currentUser =  useSelector(selectCurrentuser);
   const { isCartOpen } = useContext(CartContext);
 
   return (
