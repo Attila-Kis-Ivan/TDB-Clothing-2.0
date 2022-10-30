15269770ba389329585d32ea6ee7c59c94e2745f
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2022-10-30 09:13:50 +0000

    added stripe and created payment form

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import {store, persistor} from './store/store'
+import {stripePromise} from './utils/stripe/stripe.utils'
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,7 +18,9 @@ root.render(
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
+       <Elements stripe={stripePromise}>
         <App />
+        </Elements> 
       </BrowserRouter>
      </PersistGate>
     </Provider>
---
+++ src/utils/stripe/stripe.utils.js
@@ -0,0 +1,3 @@
+import { loadStripe } from "@stripe/stripe-js";
+
+export const stripePromise = loadStripe('pk_test_51LyFwhAWJyWrIN2g2j8qOPiudE1Pdm2n3LMmRRJpPggXC5MNPVDkev9IstAFxMaBq9VSzqFhdXnNbTpyR3YTzR8j00HoikxzkR');
