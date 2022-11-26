51d2a72b641e160a93b95a93ac597fb4ff594066
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2022-11-26 09:26:26 +0000

    added spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,15 +1,17 @@
-import ProductCard from "../../components/product-card/product-card.component";
 import { useState, useEffect, Fragment } from "react";
 import {useSelector} from 'react-redux'
 import { useParams } from "react-router-dom";
+import { selectCategoriesMap, selectCaategoriesIsLoading } from "../../store/categories/category.selector";
 
-import { selectCategoriesMap } from "../../store/categories/category.selector";
+import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import { CategoryContainer, Title } from "./category.styles";
 
 const Category = () => {
   const { category } = useParams();
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectCaategoriesIsLoading)
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,13 +21,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-
-      <CategoryContainer>
+      {
+        isLoading ? <Spinner/> :       <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
       </CategoryContainer>
+      }
     </Fragment>
   );
 };
