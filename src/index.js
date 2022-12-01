bb2a582a035ecb189b5aa592d1962eba369e9ba0
Attila Kis-Ivan <attilakis-ivan@Attilas-iMac.local>
2022-12-01 15:52:04 +0000

    added webservice

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
-import {store, persistor} from './store/store'
-import {stripePromise} from './utils/stripe/stripe.utils'
+import {store, persistor} from './store/store';
+import {stripePromise} from './utils/stripe/stripe.utils';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import "./index.scss";
 
@@ -27,6 +28,7 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
